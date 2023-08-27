7efe5cea29fa0cd2f2d8d9d30103d266e94e7005
sujithreddydharmareddy <dharmareddysujith15@gmail.com>
2023-08-26 20:45:16 -0500

    "added hello sujith in Student"

--- Student.java
+++ Student.java
@@ -2,5 +2,6 @@ public class Student {
     public static void main(String[] args) {
         System.out.println("This is student class");
         System.out.println("This is Dheeraj updated Student Class");
+        System.out.println("Hello sujith");
     }
 }
